dd8a0c4889d9a4507df9fa94c162b0df88b7278d
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-23 23:24:39 +0530

    completed

---
+++ SpringJwtAuthRestApis/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+--Add initial role
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
+INSERT INTO roles(name) VALUES('ROLE_MANAGER');
+INSERT INTO roles(name) VALUES('ROLE_HR');
+INSERT INTO roles(name) VALUES('ROLE_SOFTWAREENG');
+
+
+
+
